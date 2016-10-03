3bea4a738329eadc78e0f9899d602c8be21f5c87
Dirk Gadsden <dirk@esherido.com>
2016-10-02 23:28:02 -0700

    Implement reading of file into buffer

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,11 +1,45 @@
 use std::fs;
+use std::io::{BufRead, BufReader};
 use std::path;
-use std::sync::{Arc, Mutex};
 
 pub struct File {
     path: path::PathBuf,
     file: fs::File,
 }
+
+impl File {
+    pub fn from_path(path: &path::Path) -> File {
+        let file = fs::File::open(path).expect("could not open file");
+
+        File {
+            path: path.to_path_buf(),
+            file: file,
+        }
+    }
+
+    /// Read the whole file into lines.
+    fn read_lines(&self) -> Vec<Line> {
+        let ref file = self.file;
+        let reader = BufReader::new(file);
+
+        reader.lines()
+            .filter(|line| {
+                match line {
+                    &Ok(_) => true,
+                    &Err(ref err) => {
+                        println!("error reading line: {:?}", err);
+                        false
+                    }
+                }
+            })
+            .map(|line| {
+                let chars = line.unwrap().chars().collect();
+                Line { chars: chars }
+            })
+            .collect()
+    }
+}
+
 
 pub struct Line {
     chars: Vec<char>,
@@ -13,6 +47,15 @@ pub struct Line {
 
 /// Internal storage of a file for editing.
 pub struct Buffer {
-    pub lines: Vec<Line>,
     pub file: File,
+    pub lines: Vec<Line>,
+}
+
+impl Buffer {
+    pub fn from_file(file: File) -> Buffer {
+        Buffer {
+            lines: file.read_lines(),
+            file: file,
+        }
+    }
 }
