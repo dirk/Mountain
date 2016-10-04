4e3c49b08b355221d76bfeaf5a65191ad28811c9
Dirk Gadsden <dirk@esherido.com>
2016-10-04 00:33:18 -0700

    Start building functionality for opening paths in a workspace

--- src/pane.rs
+++ src/pane.rs
@@ -1,9 +1,28 @@
-use buffer::Buffer;
+use std::convert::AsRef;
+use std::path::Path;
+
+use buffer::{Buffer, File};
 
 pub struct Pane {
     items: Vec<Item>,
 }
 
+impl Pane {
+    pub fn add_item(&mut self, item: Item) {
+        self.items.push(item)
+    }
+}
+
 pub struct Item {
     buffer: Buffer,
+}
+
+impl Item {
+    pub fn from_path<P: AsRef<Path>>(path: P) -> Item {
+        let buffer = Buffer::from_file(File::from_path(path.as_ref()));
+
+        Item {
+            buffer: buffer,
+        }
+    }
 }
--- src/workspace.rs
+++ src/workspace.rs
@@ -1,9 +1,9 @@
-use std::path;
+use std::path::{Path, PathBuf};
 
-use pane::Pane;
+use pane::{Item, Pane};
 
 struct Project {
-    directory: path::PathBuf,
+    directory: PathBuf,
 }
 
 struct Window;
@@ -13,4 +13,15 @@ struct Workspace {
     /// Window to which the `Workspace` is rendered.
     window: Window,
     panes: [Pane; 1],
+}
+
+impl Workspace {
+    pub fn active_pane_mut<'a>(&'a mut self) -> &'a mut Pane {
+        &mut self.panes[0]
+    }
+
+    pub fn open_path<P: AsRef<Path>>(&mut self, path: P) {
+        let mut pane = self.active_pane_mut();
+        pane.add_item(Item::from_path(path))
+    }
 }
