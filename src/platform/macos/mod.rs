e3f4632393f542d1bf383e53acdd0cecdace31b1
Dirk Gadsden <dirk@esherido.com>
2016-10-11 00:54:11 -0700

    Make organization of menu setup a little clearer

--- src/platform/macos/mod.rs
+++ src/platform/macos/mod.rs
@@ -32,12 +32,15 @@ impl Application {
     // Boilerplate menu setup. Cribbed from:
     //   https://github.com/servo/cocoa-rs/blob/master/examples/hello_world.rs
     unsafe fn setup_menu(&self) {
-        let menubar = NSMenu::new(nil).autorelease();
+        let main_menu = NSMenu::new(nil).autorelease();
+        self.app.setMainMenu_(main_menu);
+
         let app_menu_item = NSMenuItem::new(nil).autorelease();
-        menubar.addItem_(app_menu_item);
-        self.app.setMainMenu_(menubar);
+        main_menu.addItem_(app_menu_item);
 
         let app_menu = NSMenu::new(nil).autorelease();
+        app_menu_item.setSubmenu_(app_menu);
+
         let quit_title = NSString::alloc(nil).init_str("Quit");
         let quit_action = selector("terminate:");
         let quit_key = NSString::alloc(nil).init_str("q");
@@ -47,7 +50,6 @@ impl Application {
             quit_key
         ).autorelease();
         app_menu.addItem_(quit_item);
-        app_menu_item.setSubmenu_(app_menu);
     }
 
     // Start the event loop
